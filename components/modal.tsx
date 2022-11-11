aeb7fa11369099ee39edf4dfef7ae5df7d5fd837
mmeTanya <mme.tanyalegois@gamail.com>
2022-11-11 14:28:27 +0000

    commits

--- components/modal.tsx
+++ components/modal.tsx
@@ -6,6 +6,12 @@ import s from "../styles/modal.module.scss";
 
 const Modal = ({ onClose }) => {
 
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
 
@@ -14,11 +20,6 @@ const Modal = ({ onClose }) => {
     };
   }, []);
 
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
 
   const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
