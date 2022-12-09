9f4a598ef04fd49146f8cd7ec4571558d4d16207
mmeTanya <mme.tanyalegois@gamail.com>
2022-12-09 15:31:46 +0000

    commits

--- components/picture.tsx
+++ components/picture.tsx
@@ -12,22 +12,109 @@ const Picture = () => {
     <section  >
       <div className={s.picture}>
         <Roll duration={1000}  triggerOnce={true} className={s.image}>
-          <img src="/hero1.jpg" alt="love_birds" width='100%' />
+          <img src="/image_part_001.jpg" alt="love_birds" width='100%' />
         </Roll>
         <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true} >
-          <img src="/hero2.jpg" alt="love_birds" width='100%'  />
+          <img src="/image_part_002.jpg" alt="love_birds" width='100%'  />
         </Roll>
         <Roll duration={1000}  className={s.image} triggerOnce={true} >
-          <img src="/hero3.jpg" alt="love_birds" width='100%' />
+          <img src="/image_part_003.jpg" alt="love_birds" width='100%' />
         </Roll>
         <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true}>
-          <img src="/hero4.jpg" alt="love_birds" width='100%' />
+          <img src="/image_part_004.jpg" alt="love_birds" width='100%' />
         </Roll>
         <Roll duration={1000}  className={s.image} triggerOnce={true} >
-          <img src="/hero5.jpg" alt="love_birds" width='100%' />
+          <img src="/image_part_005.jpg" alt="love_birds" width='100%' />
         </Roll>
         <Roll duration={1000}  className={s.image} direction={'right'} triggerOnce={true}>
-          <img src="/hero6.jpg" alt="love_birds" width='100%' />
+          <img src="/image_part_006.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  triggerOnce={true} className={s.image}>
+          <img src="/image_part_007.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true} >
+          <img src="/image_part_008.jpg" alt="love_birds" width='100%'  />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_009.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_010.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_011.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_012.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  triggerOnce={true} className={s.image}>
+          <img src="/image_part_013.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true} >
+          <img src="/image_part_014.jpg" alt="love_birds" width='100%'  />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_015.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_016.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_017.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_018.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  triggerOnce={true} className={s.image}>
+          <img src="/image_part_019.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true} >
+          <img src="/image_part_020.jpg" alt="love_birds" width='100%'  />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_021.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_022.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_023.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_024.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  triggerOnce={true} className={s.image}>
+          <img src="/image_part_025.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true} >
+          <img src="/image_part_026.jpg" alt="love_birds" width='100%'  />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_027.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_028.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_029.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_030.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  triggerOnce={true} className={s.image}>
+          <img src="/image_part_031.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true} >
+          <img src="/image_part_032.jpg" alt="love_birds" width='100%'  />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_033.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000} className={s.image} direction={'right'} triggerOnce={true}>
+          <img src="/image_part_034.jpg" alt="love_birds" width='100%' />
+        </Roll>
+        <Roll duration={1000}  className={s.image} triggerOnce={true} >
+          <img src="/image_part_035.jpg" alt="love_birds" width='100%' />
         </Roll>
         <Hero />
       </div>
