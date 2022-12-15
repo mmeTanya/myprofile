a72c62e434b36cf12809ac0ea55f16246d5b6a3c
mmeTanya <mme.tanyalegois@gamail.com>
2022-12-15 14:56:13 +0000

    commits

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -10,6 +10,7 @@ const About: NextPage = () => {
       <Head>
         <title>Tetiana Legois</title>
         <meta name="description" content="Profile of Full Stack web developer Tetiana Legois Windsor United Kingdom London React Javascript Next.js Node.js, HTML, CSS . Web aplication Website" />
+        <meta name="google-site-verification" content="sLgvG5I44yqfYMqCGAT3uDW7m3iseCwGCYQwqgEcShc" />
         <link rel="icon" href="/love_birds.png" />
       </Head>
       <Header />
